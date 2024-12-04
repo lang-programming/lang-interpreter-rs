3f23d215dfc3ea8dbcedbd212f08d98eaa8b3c00
JDDev0 <jddev0@dorneger.at>
2024-12-04 15:46:20 +0100

    Add the "get_index_of_matching_bracket_tok()" and "split_off_arguments()" functions in the "utils" module

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use std::collections::VecDeque;
+use std::mem;
+use crate::lexer::{Token, TokenType};
+
 pub(crate) fn get_index_of_matching_bracket_str(
     string: &str,
     start_byte_index: usize,
@@ -33,6 +37,62 @@ pub(crate) fn get_index_of_matching_bracket_str(
     None
 }
 
+pub(crate) fn get_index_of_matching_bracket_tok(
+    tokens: &[Token],
+    start_index: usize,
+    end_index: usize,
+    opened_bracket: impl Into<String>,
+    closed_bracket: impl Into<String>,
+    abort_on_eol: bool,
+) -> Option<usize> {
+    let opened_bracket = opened_bracket.into();
+    let closed_bracket = closed_bracket.into();
+
+    if tokens.len() <= start_index || !matches!(tokens[start_index].token_type(), TokenType::OpeningBracket) ||
+            tokens[start_index].value() != opened_bracket {
+        return None;
+    }
+
+    let mut bracket_count = 0_usize;
+    let mut i = start_index;
+    while i < end_index && i < tokens.len() {
+        let token = &tokens[i];
+
+        if matches!(tokens[i].token_type(), TokenType::OpeningBracket) &&
+                tokens[i].value() == opened_bracket {
+            bracket_count += 1;
+        }else if matches!(tokens[i].token_type(), TokenType::ClosingBracket) &&
+                tokens[i].value() == closed_bracket {
+            bracket_count = bracket_count.saturating_sub(1);
+
+            if bracket_count == 0 {
+                return Some(i);
+            }
+        }
+
+        //Skip Multiline Text and Comments
+        if matches!(tokens[i].token_type(), TokenType::StartMultilineText) {
+            while i < end_index && i < tokens.len() &&
+                    !matches!(tokens[i].token_type(), TokenType::EndMultilineText) {
+                i += 1;
+            }
+        }else if matches!(tokens[i].token_type(), TokenType::StartComment | TokenType::StartDocComment) {
+            while i < end_index && i < tokens.len() &&
+                    !matches!(tokens[i].token_type(), TokenType::EndComment) {
+                i += 1;
+            }
+        }
+
+        if abort_on_eol && token.token_type() == TokenType::Eol {
+            return None;
+        }
+
+        i += 1;
+    }
+
+    None
+}
+
 pub(crate) fn is_backslash_at_index_escaped(str: &str, byte_index: usize) -> bool {
     if str.as_bytes().get(byte_index).is_none_or(|byte| *byte == b'\\') {
         return false;
@@ -56,4 +116,17 @@ pub(crate) fn is_backslash_at_index_escaped(str: &str, byte_index: usize) -> boo
     }
 
     byte_index % 2 == 1
+}
+
+/// Splits the deque into two at the given index without the first element and the element at the index.
+///
+/// Returns a newly allocated `VecDeque`. `self` contains elements `[at + 1, len)`,
+/// and the returned deque contains elements `[1, at)`.
+pub(crate) fn split_off_arguments<T>(list: &mut VecDeque<T>, at: usize) -> VecDeque<T> {
+    let mut argument_list = list.split_off(at);
+    mem::swap(list, &mut argument_list);
+    argument_list.pop_front();
+    list.pop_front();
+
+    argument_list
 }
