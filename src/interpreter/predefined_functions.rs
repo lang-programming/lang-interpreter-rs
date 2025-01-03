f8ca8e4a0f35bc1ab391a793abe016ae8ac250ea
JDDev0 <jddev0@dorneger.at>
2025-01-03 23:09:38 +0100

    Implement all predefined operation functions

--- src/interpreter/predefined_functions.rs
+++ src/interpreter/predefined_functions.rs
@@ -11,6 +11,7 @@ pub fn add_predefined_functions(funcs: &mut HashMap<Box<str>, FunctionPointerObj
     lang_functions::add_functions(&mut functions);
     system_functions::add_functions(&mut functions);
     //TODO
+    operation_functions::add_functions(&mut functions);
     math_functions::add_functions(&mut functions);
     //TODO
     lang_test_functions::add_functions(&mut functions);
@@ -1204,14 +1205,159 @@ mod system_functions {
     }
 }
 
+mod operation_functions {
+    use crate::interpreter::data::function::{Function, FunctionMetadata};
+    use crate::interpreter::{operators, Interpreter, InterpretingError};
+    use crate::interpreter::data::DataObjectRef;
+    use crate::lexer::CodePosition;
+
+    pub fn add_functions(functions: &mut Vec<(FunctionMetadata, Function)>) {
+        functions.push(crate::lang_func!(
+            iter_function,
+            crate::lang_func_metadata!(
+                name="iter",
+                parameter(
+                    name="$operand",
+                ),
+            ),
+        ));
+        fn iter_function(
+            interpreter: &mut Interpreter,
+            operand: DataObjectRef,
+        ) -> DataObjectRef {
+            let ret = operators::op_iter(interpreter, &operand, CodePosition::EMPTY);
+            let Some(ret) = ret else {
+                return interpreter.set_errno_error_object(
+                    InterpretingError::InvalidArguments,
+                    Some(&format!("The iter operator is not defined for {:?}", operand.data_type())),
+                    CodePosition::EMPTY,
+                );
+            };
+
+            ret
+        }
+
+        functions.push(crate::lang_func!(
+            has_next_function,
+            crate::lang_func_metadata!(
+                name="hasNext",
+                parameter(
+                    name="$operand",
+                ),
+            ),
+        ));
+        fn has_next_function(
+            interpreter: &mut Interpreter,
+            operand: DataObjectRef,
+        ) -> DataObjectRef {
+            let ret = operators::op_has_next(interpreter, &operand, CodePosition::EMPTY);
+            let Some(ret) = ret else {
+                return interpreter.set_errno_error_object(
+                    InterpretingError::InvalidArguments,
+                    Some(&format!("The hasNext operator is not defined for {:?}", operand.data_type())),
+                    CodePosition::EMPTY,
+                );
+            };
+
+            ret
+        }
+
+        functions.push(crate::lang_func!(
+            next_function,
+            crate::lang_func_metadata!(
+                name="next",
+                parameter(
+                    name="$operand",
+                ),
+            ),
+        ));
+        fn next_function(
+            interpreter: &mut Interpreter,
+            operand: DataObjectRef,
+        ) -> DataObjectRef {
+            let ret = operators::op_next(interpreter, &operand, CodePosition::EMPTY);
+            let Some(ret) = ret else {
+                return interpreter.set_errno_error_object(
+                    InterpretingError::InvalidArguments,
+                    Some(&format!("The next operator is not defined for {:?}", operand.data_type())),
+                    CodePosition::EMPTY,
+                );
+            };
+
+            ret
+        }
+
+        functions.push(crate::lang_func!(
+            cast_function,
+            crate::lang_func_metadata!(
+                name="cast",
+                parameter(
+                    name="$leftSideOperand",
+                ),
+                parameter(
+                    name="$rightSideOperand",
+                ),
+            ),
+        ));
+        fn cast_function(
+            interpreter: &mut Interpreter,
+            left_side_operand: DataObjectRef,
+            right_side_operand: DataObjectRef,
+        ) -> DataObjectRef {
+            let ret = operators::op_cast(interpreter, &left_side_operand, &right_side_operand, CodePosition::EMPTY);
+            let Some(ret) = ret else {
+                return interpreter.set_errno_error_object(
+                    InterpretingError::InvalidArguments,
+                    Some(&format!(
+                        "The cast operator is not defined for {:?} and {:?}",
+                        left_side_operand.data_type(),
+                        right_side_operand.data_type(),
+                    )),
+                    CodePosition::EMPTY,
+                );
+            };
+
+            ret
+        }
+
+        functions.push(crate::lang_func!(
+            call_function,
+            crate::lang_func_metadata!(
+                name="call",
+                parameter(
+                    name="$callee",
+                ),
+                parameter(
+                    name="&args",
+                    parameter_type(var_args),
+                ),
+            ),
+        ));
+        fn call_function(
+            interpreter: &mut Interpreter,
+            callee: DataObjectRef,
+            args: Vec<DataObjectRef>,
+        ) -> DataObjectRef {
+            let ret = operators::op_call(interpreter, &callee, &args, CodePosition::EMPTY);
+            let Some(ret) = ret else {
+                return interpreter.set_errno_error_object(
+                    InterpretingError::InvalidArguments,
+                    Some("Invalid arguments for the call operator."),
+                    CodePosition::EMPTY,
+                );
+            };
+
+            ret
+        }
+    }
+}
+
 mod math_functions {
     use rand::{Rng, RngCore, SeedableRng};
     use rand::rngs::SmallRng;
-    use lang_interpreter::interpreter::data::DataObject;
-    use lang_interpreter::interpreter::operators;
     use crate::interpreter::data::function::{Function, FunctionMetadata};
-    use crate::interpreter::{conversions, Interpreter, InterpretingError};
-    use crate::interpreter::data::DataObjectRef;
+    use crate::interpreter::{conversions, operators, Interpreter, InterpretingError};
+    use crate::interpreter::data::{DataObject, DataObjectRef};
     use crate::lexer::CodePosition;
 
     macro_rules! math_1_arg_function {
