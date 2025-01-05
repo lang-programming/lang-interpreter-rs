987207de6c185c9bffeb83cf182c72e10e62b6f4
JDDev0 <jddev0@dorneger.at>
2025-01-05 11:48:30 +0100

    Implement all predefined function pointer functions

--- src/interpreter/predefined_functions.rs
+++ src/interpreter/predefined_functions.rs
@@ -17,7 +17,7 @@ pub fn add_predefined_functions(funcs: &mut HashMap<Box<str>, FunctionPointerObj
     operation_functions::add_functions(&mut functions);
     math_functions::add_functions(&mut functions);
     combinator_functions::add_functions(&mut functions);
-    //TODO
+    func_ptr_functions::add_functions(&mut functions);
     byte_buffer_functions::add_functions(&mut functions);
     array_functions::add_functions(&mut functions);
     //TODO
@@ -4780,6 +4780,383 @@ mod combinator_functions {
             ).unwrap();
 
             operators::op_call(interpreter, &ret_anon_func_1, &[ret_anon_func_2], CodePosition::EMPTY)
+        }
+    }
+}
+
+mod func_ptr_functions {
+    use std::rc::Rc;
+    use crate::interpreter::data::function::{Function, FunctionMetadata, FunctionPointerObject};
+    use crate::interpreter::{operators, Interpreter};
+    use crate::interpreter::data::{DataObject, DataObjectRef, OptionDataObjectRef};
+    use crate::lexer::CodePosition;
+
+    pub fn add_functions(functions: &mut Vec<(FunctionMetadata, Function)>) {
+        functions.push(crate::lang_func!(
+            arg_cnt_0_function,
+            crate::lang_func_metadata!(
+                name="argCnt0",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_0_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_0_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_0_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_0_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt0-func",
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_0_func_function.copy_with_function_name(&format!(
+                    "<argCnt0({function_name})>",
+                ))))
+            }).unwrap())
+        }
+
+        functions.push(crate::lang_func!(
+            arg_cnt_1_function,
+            crate::lang_func_metadata!(
+                name="argCnt1",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_1_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_1_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter, a: DataObjectRef| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[
+                        a,
+                    ], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_1_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_1_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt1-func",
+                    parameter(
+                        name="$a",
+                    ),
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_1_func_function.copy_with_function_name(&format!(
+                    "<argCnt1({function_name})>",
+                ))))
+            }).unwrap())
+        }
+
+        functions.push(crate::lang_func!(
+            arg_cnt_2_function,
+            crate::lang_func_metadata!(
+                name="argCnt2",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_2_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_2_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter, a: DataObjectRef, b: DataObjectRef| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[
+                        a, b,
+                    ], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_2_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_2_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt2-func",
+                    parameter(
+                        name="$a",
+                    ),
+                    parameter(
+                        name="$b",
+                    ),
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_2_func_function.copy_with_function_name(&format!(
+                    "<argCnt2({function_name})>",
+                ))))
+            }).unwrap())
+        }
+
+        functions.push(crate::lang_func!(
+            arg_cnt_3_function,
+            crate::lang_func_metadata!(
+                name="argCnt3",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_3_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_3_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter, a: DataObjectRef, b: DataObjectRef, c: DataObjectRef| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[
+                        a, b, c,
+                    ], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_3_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_3_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt3-func",
+                    parameter(
+                        name="$a",
+                    ),
+                    parameter(
+                        name="$b",
+                    ),
+                    parameter(
+                        name="$c",
+                    ),
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_3_func_function.copy_with_function_name(&format!(
+                    "<argCnt3({function_name})>",
+                ))))
+            }).unwrap())
+        }
+
+        functions.push(crate::lang_func!(
+            arg_cnt_4_function,
+            crate::lang_func_metadata!(
+                name="argCnt4",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_4_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_4_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter, a: DataObjectRef, b: DataObjectRef, c: DataObjectRef, d: DataObjectRef| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[
+                        a, b, c, d,
+                    ], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_4_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_4_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt4-func",
+                    parameter(
+                        name="$a",
+                    ),
+                    parameter(
+                        name="$b",
+                    ),
+                    parameter(
+                        name="$c",
+                    ),
+                    parameter(
+                        name="$d",
+                    ),
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_4_func_function.copy_with_function_name(&format!(
+                    "<argCnt4({function_name})>",
+                ))))
+            }).unwrap())
+        }
+
+        functions.push(crate::lang_func!(
+            arg_cnt_5_function,
+            crate::lang_func_metadata!(
+                name="argCnt5",
+                return_type_constraint(
+                    allowed=["FUNCTION_POINTER"],
+                ),
+                parameter(
+                    name="$func",
+                    parameter_type(callable),
+                ),
+            ),
+        ));
+        fn arg_cnt_5_function(
+            _: &mut Interpreter,
+            func_object: DataObjectRef,
+        ) -> DataObjectRef {
+            let function_name = if let Some(function_value) = func_object.function_pointer_value() {
+                let function_name = function_value.function_name();
+                if let Some(function_name) = function_name {
+                    function_name.to_string()
+                }else {
+                    func_object.variable_name().map(|str| str.to_string()).unwrap_or_else(|| "null".to_string())
+                }
+            }else {
+                "<arg>".to_string()
+            };
+
+            let arg_cnt_5_func_function = {
+                let func_object = func_object.clone();
+
+                move |interpreter: &mut Interpreter, a: DataObjectRef, b: DataObjectRef, c: DataObjectRef, d: DataObjectRef, e: DataObjectRef| -> OptionDataObjectRef {
+                    operators::op_call(interpreter, &func_object, &[
+                        a, b, c, d, e,
+                    ], CodePosition::EMPTY)
+                }
+            };
+            let arg_cnt_5_func_function = FunctionPointerObject::from(crate::lang_func!(
+                arg_cnt_5_func_function,
+                vec![
+                    Box::new(func_object),
+                ],
+                crate::lang_func_metadata!(
+                    name="argCnt4-func",
+                    parameter(
+                        name="$a",
+                    ),
+                    parameter(
+                        name="$b",
+                    ),
+                    parameter(
+                        name="$c",
+                    ),
+                    parameter(
+                        name="$d",
+                    ),
+                    parameter(
+                        name="$e",
+                    ),
+                ),
+            ));
+
+            DataObjectRef::new(DataObject::with_update(|data_object| {
+                data_object.set_function_pointer(Rc::new(arg_cnt_5_func_function.copy_with_function_name(&format!(
+                    "<argCnt5({function_name})>",
+                ))))
+            }).unwrap())
         }
     }
 }
