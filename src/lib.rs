16f2e474601c7090ce0efe434c4e03b0fab2b3e6
JDDev0 <jddev0@dorneger.at>
2025-01-06 15:25:44 +0100

    Add documentation for all macros

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,24 @@ pub mod interpreter;
 extern crate self as lang_interpreter;
 
 extern crate lang_interpreter_macros;
+
+/// This macro generates a unique ID for Lang Native Function
+///
+/// This macro is used within the [crate::lang_func_adapter!] macro
 pub use lang_interpreter_macros::lang_func_id;
+
+/// This macro generates an instance of [FunctionMetadata](crate::interpreter::data::function::FunctionMetadata)
+///
+/// It should be used in combination with [crate::lang_func!]
 pub use lang_interpreter_macros::lang_func_metadata;
+
+/// This macro generates a boxed [NativeFunctionAdapter](crate::interpreter::data::function::native::NativeFunctionAdapter)
+/// instance for the given function identifier and metadata combination
+///
+/// The return type of this macro is `(FunctionMetadata, Box<dyn NativeFunctionAdapter>, NativeFuncId)`
+///
+/// This macro is used within the [crate::lang_func!] macro
 pub use lang_interpreter_macros::lang_func_adapter;
+
 use lang_interpreter_macros::internal_tuple_from_lang_args_impl;
 use lang_interpreter_macros::internal_native_function_adapter_impl;
--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,141 @@
+/// This macro generates a [Function](crate::interpreter::data::function::Function)
+/// instance for the given function identifier and metadata combination
+///
+/// The return type of this macro is `(FunctionMetadata, Box<dyn NativeFunctionAdapter>)`
+///
+/// The function provided to this macro must start with a parameter of type `&mut Interpreter` and
+/// the must be an implementation of [FromLangArgs](lang_interpreter::interpreter::data::function::native::FromLangArgs)
+/// for the tuple of all parameter types after the `&mut Interpreter`.
+///
+/// The return type of the function must one of the following:
+/// - `Result<OptionDataObjectRef, NativeError>`
+/// - `Result<DataObjectRet, NativeError>`
+/// - `Result<(), NativeError>`
+/// - `OptionDataObjectRef`
+/// - `DataObjectRef`
+/// - `()` (= No return value)
+///
+/// # Examples
+///
+/// ```
+/// use lang_interpreter::interpreter::data::{DataObject, DataObjectRef};
+/// use lang_interpreter::interpreter::data::function::{Function, FunctionMetadata, FunctionPointerObject, Parameter, ParameterType};
+/// use lang_interpreter::interpreter::{conversions, Interpreter};
+/// use lang_interpreter::{lang_func, lang_func_metadata};
+/// use lang_interpreter::lexer::CodePosition;
+///
+/// //The rust implementation of the lang native function "func.trim"
+/// fn trim_function(
+///     interpreter: &mut Interpreter,
+///     text_object: DataObjectRef,
+/// ) -> DataObjectRef {
+///     DataObjectRef::new(DataObject::new_text(
+///         conversions::to_text(interpreter, &text_object, CodePosition::EMPTY).trim(),
+///     ))
+/// }
+///
+/// let (metadata, function): (FunctionMetadata, Function) = lang_func!(
+///     trim_function,
+///     lang_func_metadata!(
+///         name="trim",
+///         return_type_constraint(
+///             allowed=["TEXT"],
+///         ),
+///         parameter(
+///             name="$text",
+///         ),
+///     ),
+/// );
+/// ```
+///
+/// ```
+/// # use lang_interpreter::interpreter::data::{DataObject, DataObjectRef};
+/// # use lang_interpreter::interpreter::data::function::{Function, FunctionMetadata, Parameter, ParameterType};
+/// # use lang_interpreter::interpreter::{conversions, Interpreter};
+/// # use lang_interpreter::{lang_func, lang_func_metadata};
+/// # use lang_interpreter::lexer::CodePosition;
+/// #
+/// # fn trim_function(
+/// #     interpreter: &mut Interpreter,
+/// #     text_object: DataObjectRef,
+/// # ) -> DataObjectRef {
+/// #     DataObjectRef::new(DataObject::new_text(
+/// #         conversions::to_text(interpreter, &text_object, CodePosition::EMPTY).trim(),
+/// #     ))
+/// # }
+/// #
+/// # let (metadata, function): (FunctionMetadata, Function) = lang_func!(
+/// #     trim_function,
+/// #     lang_func_metadata!(
+/// #         name="trim",
+/// #         return_type_constraint(
+/// #             allowed=["TEXT"],
+/// #         ),
+/// #         parameter(
+/// #             name="$text",
+/// #         ),
+/// #     ),
+/// # );
+/// #
+/// use lang_interpreter::interpreter::data::function::FunctionPointerObject;
+///
+/// //`(metadata, function)` is used from the example above
+///
+/// // FunctionPointerObject::new can be used to create a FunctionPointerObject with a single non-overloaded function
+/// let fp: FunctionPointerObject = FunctionPointerObject::new(&metadata, function);
+///
+/// assert_eq!(fp.function_name(), Some("trim"));
+/// assert_eq!(fp.function_info(), None);
+/// assert_eq!(fp.functions().len(), 1);
+/// ```
+///
+/// ```
+/// # use lang_interpreter::interpreter::data::{DataObject, DataObjectRef};
+/// # use lang_interpreter::interpreter::data::function::{Function, FunctionMetadata, Parameter, ParameterType};
+/// # use lang_interpreter::interpreter::{conversions, Interpreter};
+/// # use lang_interpreter::{lang_func, lang_func_metadata};
+/// # use lang_interpreter::lexer::CodePosition;
+/// #
+/// # fn trim_function(
+/// #     interpreter: &mut Interpreter,
+/// #     text_object: DataObjectRef,
+/// # ) -> DataObjectRef {
+/// #     DataObjectRef::new(DataObject::new_text(
+/// #         conversions::to_text(interpreter, &text_object, CodePosition::EMPTY).trim(),
+/// #     ))
+/// # }
+/// #
+/// # let (metadata, function): (FunctionMetadata, Function) = lang_func!(
+/// #     trim_function,
+/// #     lang_func_metadata!(
+/// #         name="trim",
+/// #         return_type_constraint(
+/// #             allowed=["TEXT"],
+/// #         ),
+/// #         parameter(
+/// #             name="$text",
+/// #         ),
+/// #     ),
+/// # );
+/// #
+/// use std::collections::HashMap;
+/// use lang_interpreter::interpreter::data::function::FunctionPointerObject;
+///
+/// //`(metadata, function)` is used from the example above
+///
+/// //Overloaded functions can be created with `FunctionPointerObject::create_function_pointer_objects_from_native_functions`
+/// let overloaded_functions: HashMap<Box<str>, FunctionPointerObject> = FunctionPointerObject::create_function_pointer_objects_from_native_functions(vec![
+///     (metadata, function),
+/// ]);
+///
+/// assert_eq!(overloaded_functions.len(), 1);
+///
+/// let fp = &overloaded_functions["trim"];
+///
+/// assert_eq!(fp.function_name(), Some("trim"));
+/// assert_eq!(fp.function_info(), None);
+/// assert_eq!(fp.functions().len(), 1);
+/// ```
 #[macro_export]
 macro_rules! lang_func {
     ( $func:ident, $metadata:expr $(,)? ) => {{
