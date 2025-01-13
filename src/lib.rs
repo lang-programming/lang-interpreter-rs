7b6cbcfff44c2139d2f9e33e0e048a4727e5ad0d
JDDev0 <jddev0@dorneger.at>
2025-01-13 17:38:45 +0100

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@
 #![warn(clippy::suboptimal_flops)]
 #![warn(clippy::too_long_first_doc_paragraph)]
 
-//Triggered by for Trace and Finalize derive
+//Triggered by Trace and Finalize derive
 #![allow(non_local_definitions)]
 
 pub(crate) mod regex_patterns;
